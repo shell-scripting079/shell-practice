806d93836b0feac9567ad7522a8cd05a020ac233
shell-scripting079 <sravs3629@gmail.com>
2026-05-22 10:19:45 +0530

    Delete 01.conversation.sh

--- 01.conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Ramesh:: Hi Suresh ,How are you?"
-echo "suresh:: Hello Ramesh, I am fine. How are you?"
-echo "Ramesh:: What are you learning recently?"
-echo "suresh:: I am learning Shell scripting Ramesh"
