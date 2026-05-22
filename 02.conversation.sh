6a35ea80792fc69b27c83fb556ffe50c5f3edb8b
unknown <sravs3629@gmail.com>
2026-05-22 10:00:17 +0530

    hello world started

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh ,How are you?"
+echo "suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "suresh:: I am learning Shell scripting Ramesh"
