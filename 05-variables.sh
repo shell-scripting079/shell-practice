d788a791ed6cbc486f8b18b2cc4bbd9966b0477a
venkatesh <sravs3629@gmail.com>
2026-05-31 06:16:52 +0530

    some message

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Please enter your PIN Number"
-read PIN _NUMBER # here PIN_NUMBER is variable"
+read -s PIN _NUMBER # here PIN_NUMBER is variable"
 
 echo "PIN Entered is: $PIN_NUMBER"
 
