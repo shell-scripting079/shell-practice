d15c0696960864198cdb2dec68298d6f0b34f9fe
venkatesh <sravs3629@gmail.com>
2026-05-31 08:30:58 +0530

    some message

--- 08-special-varis.sh
+++ 08-special-varis.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID pf this script: $$"
+sleep 50 &
+
