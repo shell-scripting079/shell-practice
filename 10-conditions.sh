4837f4a5f8df8e46dedab0e769af306e2e27fd2d
venkatesh <sravs3629@gmail.com>
2026-05-31 23:51:46 +0530

    some message

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -6,7 +6,7 @@ if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is lessthen 10"
 elif [ $NUMBER -eq 10 ]; then
   echo "Given number $NUMBER is equal to 10"
-else [ $NUMBER -gt 10 ]; then 
+else 
   echo "Given number $NUMBER is greaterthen or eual to 10"
 fi
 
